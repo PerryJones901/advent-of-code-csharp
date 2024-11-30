da42f5688d598affc1ccd1798d148f7cc3b625e4
Perry Jones <perry.jones@softwire.com>
2024-11-30 09:28:36 +0100

    Added DayTests (Template file)

---
+++ AdventOfCode2024.Tests/Days/DayTests.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2024.Tests.Days
+{
+    internal class DayTests
+    {
+        [SetUp]
+        public void Setup() { }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
