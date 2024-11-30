a309e10cc1d3af18334fadc153499f11fd1f9864
Perry Jones <perry.jones@softwire.com>
2024-11-30 09:27:34 +0100

    Added DayBase

---
+++ AdventOfCode2024/Days/DayBase.cs
@@ -0,0 +1,19 @@
+﻿using AdventOfCodeCommon;
+
+namespace AdventOfCode2024.Days;
+
+public abstract class DayBase(int dayNumber, bool isTest = false)
+{
+    // TODO: Don't hardcode this path
+    private const string INPUT_FILES_PATH = "C:\\Work\\advent-of-code-csharp\\AdventOfCode2024\\InputFiles";
+
+    protected string FileNameSuffix => IsTest ? "_Test" : "";
+    protected string TextInputFileName => $"Day{DayNumber.ToString().PadLeft(2, '0')}{FileNameSuffix}";
+    protected string TextInputFilePath => $"{INPUT_FILES_PATH}\\{TextInputFileName}.txt";
+
+    protected readonly int DayNumber = dayNumber;
+    protected readonly bool IsTest = isTest;
+
+    public abstract string Part1();
+    public abstract string Part2();
+}
