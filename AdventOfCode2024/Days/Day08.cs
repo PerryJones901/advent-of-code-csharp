1010ebcdf2397fe089bd308f2e3e6bbb2cfe997b
Perry Jones <perry.jones@softwire.com>
2024-12-08 08:46:18 +0100

    Day08 refactor

--- AdventOfCode2024/Days/Day08.cs
+++ AdventOfCode2024/Days/Day08.cs
@@ -9,35 +9,30 @@ namespace AdventOfCode2024.Days
             var input = GetInput();
             var frequenciesToLocations = GetFrequenciesToLocationsDictionary(input);
 
-            var visitedLocations = new HashSet<(int, int)>();
+            var antiNodeLocations = new HashSet<(int, int)>();
 
             foreach (var frequencyToLocations in frequenciesToLocations)
             {
                 var locations = frequencyToLocations.Value;
-                for (int firstLocationIndex = 0; firstLocationIndex < locations.Count; firstLocationIndex++)
+
+                foreach (var (firstLocation, secondLocation) in GetAllPairCombinations(locations))
                 {
-                    for (int secondLocationIndex = firstLocationIndex + 1; secondLocationIndex < locations.Count; secondLocationIndex++)
-                    {
-                        var firstLocation = locations[firstLocationIndex];
-                        var secondLocation = locations[secondLocationIndex];
+                    var rowDiff = secondLocation.Item1 - firstLocation.Item1;
+                    var colDiff = secondLocation.Item2 - firstLocation.Item2;
 
-                        var rowDiff = secondLocation.Item1 - firstLocation.Item1;
-                        var colDiff = secondLocation.Item2 - firstLocation.Item2;
+                    var antinode1Row = secondLocation.Item1 + rowDiff;
+                    var antinode1Col = secondLocation.Item2 + colDiff;
+                    if (IsInBounds(antinode1Row, antinode1Col, input))
+                        antiNodeLocations.Add((antinode1Row, antinode1Col));
 
-                        var antinode1Row = secondLocation.Item1 + rowDiff;
-                        var antinode1Col = secondLocation.Item2 + colDiff;
-                        if (IsInBounds(antinode1Row, antinode1Col, input))
-                            visitedLocations.Add((antinode1Row, antinode1Col));
-
-                        var antinode2Row = firstLocation.Item1 - rowDiff;
-                        var antinode2Col = firstLocation.Item2 - colDiff;
-                        if (IsInBounds(antinode2Row, antinode2Col, input))
-                            visitedLocations.Add((antinode2Row, antinode2Col));
-                    }
+                    var antinode2Row = firstLocation.Item1 - rowDiff;
+                    var antinode2Col = firstLocation.Item2 - colDiff;
+                    if (IsInBounds(antinode2Row, antinode2Col, input))
+                        antiNodeLocations.Add((antinode2Row, antinode2Col));
                 }
             }
 
-            var uniqueLocationCount = visitedLocations.Count;
+            var uniqueLocationCount = antiNodeLocations.Count;
 
             return uniqueLocationCount.ToString();
         }
@@ -47,55 +42,50 @@ namespace AdventOfCode2024.Days
             var input = GetInput();
             var frequenciesToLocations = GetFrequenciesToLocationsDictionary(input);
 
-            var visitedLocations = new HashSet<(int, int)>();
+            var antiNodeLocations = new HashSet<(int, int)>();
 
             foreach (var frequencyToLocations in frequenciesToLocations)
             {
                 var locations = frequencyToLocations.Value;
-                for (int firstLocationIndex = 0; firstLocationIndex < locations.Count; firstLocationIndex++)
+
+                foreach (var (first, second) in GetAllPairCombinations(locations))
                 {
-                    for (int secondLocationIndex = firstLocationIndex + 1; secondLocationIndex < locations.Count; secondLocationIndex++)
+                    var rowDiff = second.Item1 - first.Item1;
+                    var colDiff = second.Item2 - first.Item2;
+
+                    // Add nodes after second location
+                    var antinodeRow = second.Item1;
+                    var antinodeCol = second.Item2;
+
+                    while (true)
                     {
-                        var firstLocation = locations[firstLocationIndex];
-                        var secondLocation = locations[secondLocationIndex];
+                        antiNodeLocations.Add((antinodeRow, antinodeCol));
 
-                        var rowDiff = secondLocation.Item1 - firstLocation.Item1;
-                        var colDiff = secondLocation.Item2 - firstLocation.Item2;
+                        antinodeRow += rowDiff;
+                        antinodeCol += colDiff;
 
-                        // Add nodes after second location
-                        var antinodeRow = secondLocation.Item1;
-                        var antinodeCol = secondLocation.Item2;
+                        if (!IsInBounds(antinodeRow, antinodeCol, input))
+                            break;
+                    }
 
-                        while (true)
-                        {
-                            visitedLocations.Add((antinodeRow, antinodeCol));
+                    // Add nodes before first location
+                    antinodeRow = first.Item1;
+                    antinodeCol = first.Item2;
 
-                            antinodeRow += rowDiff;
-                            antinodeCol += colDiff;
+                    while (true)
+                    {
+                        antiNodeLocations.Add((antinodeRow, antinodeCol));
 
-                            if (!IsInBounds(antinodeRow, antinodeCol, input))
-                                break;
-                        }
+                        antinodeRow -= rowDiff;
+                        antinodeCol -= colDiff;
 
-                        // Add nodes before first location
-                        antinodeRow = firstLocation.Item1;
-                        antinodeCol = firstLocation.Item2;
-
-                        while (true)
-                        {
-                            visitedLocations.Add((antinodeRow, antinodeCol));
-
-                            antinodeRow -= rowDiff;
-                            antinodeCol -= colDiff;
-
-                            if (!IsInBounds(antinodeRow, antinodeCol, input))
-                                break;
-                        }
+                        if (!IsInBounds(antinodeRow, antinodeCol, input))
+                            break;
                     }
                 }
             }
 
-            var uniqueLocationCount = visitedLocations.Count;
+            var uniqueLocationCount = antiNodeLocations.Count;
 
             return uniqueLocationCount.ToString();
         }
@@ -119,6 +109,11 @@ namespace AdventOfCode2024.Days
             }
 
             return frequenciesToLocations;
+        }
+
+        private static IEnumerable<(T, T)> GetAllPairCombinations<T>(IList<T> enumerable)
+        {
+            return enumerable.SelectMany((item, index) => enumerable.Skip(index + 1), (x, y) => (x, y));
         }
 
         private static bool IsInBounds(int row, int col, string[] input)
