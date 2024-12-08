2e54c2c31d9cb2f50ed3c43711689d9f0ca75622
Perry Jones <perry.jones@softwire.com>
2024-12-08 09:59:34 +0100

    Added summary comments

--- AdventOfCodeCommon/Extensions/ListExtensions.cs
+++ AdventOfCodeCommon/Extensions/ListExtensions.cs
@@ -2,9 +2,15 @@
 {
     public static class ListExtensions
     {
+        /// <summary>
+        /// Determines whether the specified row and column are within the bounds of the grid.
+        /// </summary>
         public static bool IsInBounds<S, T>(this IList<T> grid, int row, int col) where T : IList<S>
             => row >= 0 && row < grid.Count && col >= 0 && col < grid[0].Count;
 
+        /// <summary>
+        /// Determines whether the specified row and column are within the bounds of the grid.
+        /// </summary>
         public static bool IsInBounds(this IList<string> grid, int row, int col)
             => row >= 0 && row < grid.Count && col >= 0 && col < grid[0].Length;
     }
