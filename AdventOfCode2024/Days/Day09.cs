8580d0781999b821b025d013f44b421ab3e7d1a9
Perry Jones <perry.jones@yuki.nl>
2024-12-09 23:34:58 +0100

    Added more Day09 WIP (with comments, will come back to this)

--- AdventOfCode2024/Days/Day09.cs
+++ AdventOfCode2024/Days/Day09.cs
@@ -6,29 +6,26 @@ namespace AdventOfCode2024.Days
     {
         public override string Part1()
         {
-            // Too high: 24249769251776
             var input = GetInput();
             var diskInfos = input.ToCharArray().Select((c, index) => new DiskInfo
             {
                 Id = index % 2 == 0 ? index / 2 : null,
                 Size = int.Parse(c.ToString())
             }).ToList();
+            var diskSize = diskInfos.Sum(x => x.Size);
 
             var files = diskInfos.Where(d => d.Id.HasValue).ToList();
-            var diskSize = diskInfos.Sum(x => x.Size);
+            var filesSize = files.Sum(x => x.Size);
 
             // End File
             var currentEndFileIndex = files.Count - 1;
             var currentEndFileByteIndex = 0;
 
             var ascendingDiskIndex = 0;
-            var descendingDiskIndex = 0;
             var checksum = 0L;
 
             foreach(var diskInfo in diskInfos)
             {
-                // TODO: Add some logic about ascending and desc = length of disk
-
                 for (int i = 0; i < diskInfo.Size; i++)
                 {
                     // If file, need to add to checksum
@@ -45,26 +42,29 @@ namespace AdventOfCode2024.Days
                         // Now, check if we must move to next end file
                         if (currentEndFileByteIndex == endFile.Size)
                         {
-                            if (endFile.Size == 0) throw new Exception("End file size is 0");
-
-                            currentEndFileIndex--;
+                            // Find next file that has non-zero size
                             currentEndFileByteIndex = 0;
-                            endFile = files[currentEndFileIndex];
+
+                            do
+                            {
+                                currentEndFileIndex--;
+                                endFile = files[currentEndFileIndex];
+                            }
+                            while (endFile.Size == 0);
                         }
 
                         // Now, we add to checksum with this end file
                         checksum += files[currentEndFileIndex].Id.Value * ascendingDiskIndex;
 
                         currentEndFileByteIndex++;
-                        descendingDiskIndex++;
                     }
 
                     ascendingDiskIndex++;
 
-                    // We are moving up by ascendingDiskIndex, and down by descendingDiskIndex
-                    // Once these meet up, we should stop
-                    if (ascendingDiskIndex == diskSize - descendingDiskIndex) break;
+                    if (ascendingDiskIndex == filesSize) break;
                 }
+
+                if (ascendingDiskIndex == filesSize) break;
             }
 
             return checksum.ToString();
@@ -73,8 +73,67 @@ namespace AdventOfCode2024.Days
         public override string Part2()
         {
             var input = GetInput();
+            var diskInfos = input.ToCharArray().Select((c, index) => new DiskInfo
+            {
+                Id = index % 2 == 0 ? index / 2 : null,
+                Size = int.Parse(c.ToString())
+            }).ToList();
 
-            return "Part 2";
+            var newDiskInfos = diskInfos.ToList();
+            var reverseFiles = diskInfos.Where(x => x.Id.HasValue).ToList();
+            reverseFiles.Reverse();
+
+            // In each iter, go from furthest right file, and see if there's a space for it near beginning
+            // If so, place it there (by removing free space, placing file then any spare space left)
+            // Keep going down file list
+            // Now, make sure we always move file LEFT and never RIGHT
+            foreach (var file in reverseFiles)
+            {
+                for (var index = 0; index < newDiskInfos.Count; index++)
+                {
+                    var item = newDiskInfos[index];
+
+                    // If NOT free space, continue
+                    if (item.Id.HasValue) continue;
+
+                    // If next file is size 0, need to do more logic, so throw for now
+                    if (index < diskInfos.Count - 1 && newDiskInfos[index + 1].Size == 0)
+                        throw new Exception("File size of 0 here");
+
+                    // Now, check for enough free space
+                    if (file.Size <= item.Size)
+                    {
+                        // We have enough!
+                        // First, remove file but replace with free space
+                        // TODO
+
+                        // First, remove free space info
+                        newDiskInfos.RemoveAt(index);
+                        newDiskInfos.Insert(index, item);
+
+                        var freeSpaceDiff = item.Size - file.Size;
+                        if (freeSpaceDiff > 0)
+                        {
+                            newDiskInfos.Insert(index + 1, new DiskInfo { Size = freeSpaceDiff });
+                        }
+
+                        break;
+                    }
+                }
+            }
+
+            var diskSize = diskInfos.Sum(x => x.Size);
+
+            var files = diskInfos.Where(d => d.Id.HasValue).ToList();
+            var filesSize = files.Sum(x => x.Size);
+
+            // End File
+            var currentEndFileIndex = files.Count - 1;
+            var currentEndFileByteIndex = 0;
+            var ascendingDiskIndex = 0;
+            var checksum = 0L;
+
+            return checksum.ToString();
         }
 
         private class DiskInfo
@@ -82,6 +141,7 @@ namespace AdventOfCode2024.Days
             // If null, it's free space
             public int? Id { get; set; }
             public int Size { get; set; }
+            public bool HasMoved { get; set; } = false;
         }
 
         private string GetInput()
