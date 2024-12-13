4eaaca6af349a89e8f65b7d2415f2935d6db8edd
Perry Jones <perry.jones@yuki.nl>
2024-12-13 22:34:22 +0100

    Day13 refactor

--- AdventOfCode2024/Days/Day13.cs
+++ AdventOfCode2024/Days/Day13.cs
@@ -5,93 +5,34 @@ namespace AdventOfCode2024.Days
 {
     internal class Day13(bool isTest) : DayBase(13, isTest)
     {
-        private const int MAX_BUTTON_PRESSES = 100;
         private const long PART_TWO_OFFSET = 10000000000000;
 
-        public override string Part1()
+        public override string Part1() => GetTotalTokenCount();
+        public override string Part2() => GetTotalTokenCount(offset: PART_TWO_OFFSET);
+
+        private string GetTotalTokenCount(long offset = 0)
         {
-            // 11349 TOO LOW
-            var input = GetInput();
-            var totalTokenCount = 0L;
-
-            // For each case, we can increment A button press by 1, and calculate the presses needed on B (if possible).
-            // - If token price is smaller than those prior, remember it
-            foreach (var prizeCase in input)
-            {
-                var x = 0L;
-                var y = 0L;
-                var minTokens = long.MaxValue;
-
-                for (int aButtonPressCount = 0; aButtonPressCount <= MAX_BUTTON_PRESSES; aButtonPressCount++)
-                {
-                    // Press A button correct number of times
-                    x = prizeCase.ButtonA.X * aButtonPressCount;
-                    y = prizeCase.ButtonA.Y * aButtonPressCount;
-
-                    // Now, calculate difference to Prize
-                    var xDiff = prizeCase.Prize.X - x;
-                    var yDiff = prizeCase.Prize.Y - y;
-
-                    // Now, need to check how many times we must press B button to get correct X pos and Y pos separately
-                    // We need to check these amounts are equal
-                    // Now, to check equality, we must have bButtonPressesForX - bButtonPressesForY = 0
-                    // This is xDiff / prizeCase.ButtonB.X - yDiff / prizeCase.ButtonB.Y = 0
-                    // Then, to avoid dividing, multiply up by prizeCase.ButtonB.X * prizeCase.ButtonB.Y
-                    var buttonPressesDiff = xDiff * prizeCase.ButtonB.Y - yDiff * prizeCase.ButtonB.X;
-
-                    // Check if the amount of B Button presses do NOT line up for both X & Y
-                    if (buttonPressesDiff != 0)
-                        continue;
-
-                    // Now, check if xDiff is NOT divisible by prizeCase.ButtonB.X (needs to be a whole number of presses)
-                    if (xDiff % prizeCase.ButtonB.X != 0)
-                        continue;
-
-                    var bButtonPressAmount = xDiff / prizeCase.ButtonB.X;
-
-                    if (bButtonPressAmount > MAX_BUTTON_PRESSES)
-                        continue;
-
-                    // Woo! We're good to go
-                    var totalTokens = 3L * aButtonPressCount + bButtonPressAmount;
-
-                    // If number of tokens is less than current min, reassign
-                    if (minTokens > totalTokens)
-                        minTokens = totalTokens;
-                }
-
-                if (minTokens < int.MaxValue)
-                    totalTokenCount += minTokens;
-            }
-
-            return totalTokenCount.ToString();
-        }
-
-        public override string Part2()
-        {
-            var input = GetInput(isPart2: true);
+            var input = GetInput(offset);
             var totalTokenCount = 0L;
 
             foreach (var prizeCase in input)
             {
                 var det = prizeCase.ButtonA.X * prizeCase.ButtonB.Y - prizeCase.ButtonB.X * prizeCase.ButtonA.Y;
 
-                // if det == 0, throw exception?
-                if (det == 0) throw new Exception("Not expected!");
-
-                // Now, if a solution exists, it's unique
+                // if det == 0, assume we don't handle
+                if (det == 0) throw new Exception("det is zero");
 
                 var aButtonPressCountUnnormed = prizeCase.ButtonB.Y * prizeCase.Prize.X - prizeCase.ButtonB.X * prizeCase.Prize.Y;
                 var bButtonPressCountUnnormed = -prizeCase.ButtonA.Y * prizeCase.Prize.X + prizeCase.ButtonA.X * prizeCase.Prize.Y;
 
-                // check unnormed values are NOT divisible by det
+                // continue if unnormed values are NOT divisible by det
                 if (aButtonPressCountUnnormed % det != 0 || bButtonPressCountUnnormed % det != 0)
                     continue;
 
                 var aButtonPressCount = aButtonPressCountUnnormed / det;
                 var bButtonPressCount = bButtonPressCountUnnormed / det;
 
-                // check if either are negative
+                // continue if either are negative
                 if (aButtonPressCount < 0 || bButtonPressCount < 0)
                     continue;
 
@@ -116,7 +57,7 @@ namespace AdventOfCode2024.Days
             public long Y { get; set; }
         }
 
-        private PrizeCase[] GetInput(bool isPart2 = false)
+        private PrizeCase[] GetInput(long offset)
         {
             var input = FileInputAssistant.GetStringFromFile(TextInputFilePath);
 
@@ -125,8 +66,6 @@ namespace AdventOfCode2024.Days
                 @"Button A: X\+(\d+), Y\+(\d+)\r\nButton B: X\+(\d+), Y\+(\d+)\r\nPrize: X=(\d+), Y=(\d+)",
                 "\r\n\r\n"
             );
-
-            var offset = isPart2 ? PART_TWO_OFFSET : 0L;
 
             var prizeCases = parts.Select(x => new PrizeCase
             {
