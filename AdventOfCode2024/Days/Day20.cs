090a185d82d79d6a10a3f73d2de07f029682d009
Perry Jones <perry.jones@yuki.nl>
2024-12-26 12:28:28 +0100

    Day20 refactor (Part 1)

--- AdventOfCode2024/Days/Day20.cs
+++ AdventOfCode2024/Days/Day20.cs
@@ -1,9 +1,6 @@
 ﻿using AdventOfCodeCommon;
-using AdventOfCodeCommon.Models;
 using System.Collections.Immutable;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.Linq;
 
 namespace AdventOfCode2024.Days
 {
@@ -31,82 +28,35 @@ namespace AdventOfCode2024.Days
 
         public override string Part1()
         {
-            // Plan:
-            // 1. Use Dijkstra's to work out distances from END of the map.
-            // 2. Then, for each non-wall spaces, look at squares dist of 2 away (1 is useless). All these squares can be reached through cheats
-            // 3. Then, work out save saved when moving to these new spaces (accounting for the 2 cheated moves)
-            //      OLD TIME: dist[current]
-            //      NEW TIME: dist[newSpace] + 2
-            //      DIFF IN TIME: (dist[current]) - (dist[newSpace] + 2)
-            // 4. Increment "Super cheat" count if we save at least 100 pico secs
-            
             var input = GetInput();
-            var reachableCoords = GetReachableCoords(input);
-            var startingPoint = GetPositionOfChar(input, 'S');
-            var endingPoint = GetPositionOfChar(input, 'E');
-            var dist = reachableCoords.ToDictionary(x => x, _ => int.MaxValue);
-            var prev = reachableCoords.ToDictionary(x => x, _ => ((int, int)?)null);
+            var dijkstrasResult = GetDijkstrasResult(input);
+            var reachableSpaces = dijkstrasResult.ReachableSpaces;
+            var dist = dijkstrasResult.SpaceToDistanceFromEnd;
 
-            var remainingCoords = reachableCoords.ToHashSet();
-
-            dist[endingPoint] = 0;
-
-            // Apply Dijkstra's
-            while (remainingCoords.Count > 0)
-            {
-                var position = remainingCoords.OrderBy(x => dist[x]).First();
-                remainingCoords.Remove(position);
-
-                foreach (var (diffRow, diffCol) in DiffValues)
-                {
-                    var neighbourRow = position.Item1 + diffRow;
-                    var neighbourCol = position.Item2 + diffCol;
-
-                    // Check if out of bounds
-                    if (neighbourRow < 0 || neighbourRow >= input.Length || neighbourCol < 0 || neighbourCol >= input[0].Length)
-                        continue;
-
-                    var neighbourPosition = (neighbourRow, neighbourCol);
-
-                    // Needs to be still in remainingCoords
-                    if (!remainingCoords.Contains(neighbourPosition))
-                        continue;
-
-                    // Decide if distance is shorter
-                    var tempDist = dist[position] == int.MaxValue ? int.MaxValue : dist[position] + 1;
-                    if (tempDist < dist[neighbourPosition])
-                    {
-                        dist[neighbourPosition] = tempDist;
-                        prev[neighbourPosition] = position;
-                    }
-                }
-            }
-
-            // A "Super cheat" is a cheat that saves more than 100 picoseconds
+            // Define "Super cheat" as a cheat which saves more than 100 pico secs
             var superCheatCount = 0;
 
-            // Now, for each reachable coord, scout out spaces 2 dist away, and work out timeSave.
-            foreach (var cheatStartPosition in reachableCoords)
+            foreach (var cheatStartPosition in reachableSpaces)
             {
                 foreach (var (twoDiffRow, twoDiffCol) in TwoDiffValues)
                 {
                     var cheatEndRow = cheatStartPosition.Item1 + twoDiffRow;
                     var cheatEndCol = cheatStartPosition.Item2 + twoDiffCol;
 
-                    // Check if out of bounds
+                    // OOB Check
                     if (cheatEndRow < 0 || cheatEndRow >= input.Length || cheatEndCol < 0 || cheatEndCol >= input[0].Length)
                         continue;
 
                     var cheatEndPosition = (cheatEndRow, cheatEndCol);
 
-                    // Needs to be still in reachableCoords
-                    if (!reachableCoords.Contains(cheatEndPosition))
+                    // Check if space is unreachable
+                    if (!reachableSpaces.Contains(cheatEndPosition))
                         continue;
 
-                    // DIFF IN TIME: (dist[current]) - (dist[newSpace] + 2)
                     var timeSave = dist[cheatStartPosition] - (dist[cheatEndPosition] + 2);
 
-                    if (timeSave >= 100) superCheatCount++;
+                    if (timeSave >= 100)
+                        superCheatCount++;
                 }
             }
 
@@ -117,10 +67,8 @@ namespace AdventOfCode2024.Days
         {
             var input = GetInput();
             var reachableCoords = GetReachableCoords(input);
-            var startingPoint = GetPositionOfChar(input, 'S');
             var endingPoint = GetPositionOfChar(input, 'E');
             var dist = reachableCoords.ToDictionary(x => x, _ => int.MaxValue);
-            var prev = reachableCoords.ToDictionary(x => x, _ => ((int, int)?)null);
 
             var remainingCoords = reachableCoords.ToHashSet();
 
@@ -152,7 +100,6 @@ namespace AdventOfCode2024.Days
                     if (tempDist < dist[neighbourPosition])
                     {
                         dist[neighbourPosition] = tempDist;
-                        prev[neighbourPosition] = position;
                     }
                 }
             }
@@ -181,6 +128,60 @@ namespace AdventOfCode2024.Days
             }
 
             return superCheatCount.ToString();
+        }
+
+        private static DijkstrasResult GetDijkstrasResult(char[][] input)
+        {
+            var reachableSpaces = GetReachableCoords(input);
+            var spaceToDistanceFromEnd = reachableSpaces.ToDictionary(x => x, _ => int.MaxValue);
+
+            var endingPoint = GetPositionOfChar(input, 'E');
+            spaceToDistanceFromEnd[endingPoint] = 0;
+
+            var remainingCoords = reachableSpaces.ToHashSet();
+
+            // Apply Dijkstra's
+            while (remainingCoords.Count > 0)
+            {
+                var position = remainingCoords.OrderBy(x => spaceToDistanceFromEnd[x]).First();
+                remainingCoords.Remove(position);
+
+                foreach (var (diffRow, diffCol) in DiffValues)
+                {
+                    var neighbourRow = position.Item1 + diffRow;
+                    var neighbourCol = position.Item2 + diffCol;
+
+                    // Check if out of bounds
+                    if (neighbourRow < 0 || neighbourRow >= input.Length || neighbourCol < 0 || neighbourCol >= input[0].Length)
+                        continue;
+
+                    var neighbourPosition = (neighbourRow, neighbourCol);
+
+                    // Needs to be still in remainingCoords
+                    if (!remainingCoords.Contains(neighbourPosition))
+                        continue;
+
+                    // Decide if distance is shorter
+                    var tempDist = spaceToDistanceFromEnd[position] == int.MaxValue
+                        ? int.MaxValue
+                        : spaceToDistanceFromEnd[position] + 1;
+
+                    if (tempDist < spaceToDistanceFromEnd[neighbourPosition])
+                        spaceToDistanceFromEnd[neighbourPosition] = tempDist;
+                }
+            }
+
+            return new()
+            {
+                ReachableSpaces = reachableSpaces,
+                SpaceToDistanceFromEnd = spaceToDistanceFromEnd
+            };
+        }
+
+        private class DijkstrasResult
+        {
+            public required HashSet<(int, int)> ReachableSpaces { get; set; }
+            public required Dictionary<(int, int), int> SpaceToDistanceFromEnd { get; set; }
         }
 
         private static (int, int) GetPositionOfChar(char[][] grid, char charToFind)
