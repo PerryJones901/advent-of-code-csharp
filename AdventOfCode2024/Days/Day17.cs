9760cd2e1879ae5a1d50f276e56fcd35e3dbecde
Perry Jones <perry.jones@softwire.com>
2025-10-12 13:55:12 +0200

    Day17: Refactor

--- AdventOfCode2024/Days/Day17.cs
+++ AdventOfCode2024/Days/Day17.cs
@@ -6,13 +6,14 @@ namespace AdventOfCode2024.Days
     {
         public override string Part1()
         {
-            var input = GetInput();
-            var regA = long.Parse(input[0].Split("Register A: ")[1]);
-            var regB = long.Parse(input[1].Split("Register B: ")[1]);
-            var regC = long.Parse(input[2].Split("Register C: ")[1]);
-            var numbers = input[4].Split("Program: ")[1].Split(",").Select(int.Parse).ToArray();
+            var input = ParseInput(GetInputFromFile());
 
-            var outputValues = GetOutput(regA, regB, regC, numbers);
+            var outputValues = GetOutput(
+                input.RegA,
+                input.RegB,
+                input.RegC,
+                input.Program);
+
             var outputString = string.Join(",", outputValues);
 
             return outputString;
@@ -20,49 +21,40 @@ namespace AdventOfCode2024.Days
 
         public override string Part2()
         {
-            // TOO LOW: 64559155
-            // TOO LOW: 64559548
-            // (not offical but) TOO LOW: 2015500000
-
-            var input = GetInput();
-            var regBFromFile = long.Parse(input[1].Split("Register B: ")[1]);
-            var regCFromFile = long.Parse(input[2].Split("Register C: ")[1]);
-            var numbers = input[4].Split("Program: ")[1].Split(",").Select(int.Parse).ToArray();
+            var input = ParseInput(GetInputFromFile());
+            var program = input.Program;
 
             var regA = 0L;
-            for (int i = 1; i <= numbers.Length; i++)
+            for (int i = 1; i <= program.Length; i++)
             {
                 regA <<= 3;
 
-                var goalNumbers = numbers.TakeLast(i).ToArray();
-                while (!GetOutputPart2(regA, regBFromFile, regCFromFile, numbers).SequenceEqual(goalNumbers))
+                var goalNumbers = program.TakeLast(i).ToArray();
+                while (!GetOutput(regA, input.RegB, input.RegC, program).SequenceEqual(goalNumbers))
                     regA++;
             }
 
             return regA.ToString();
         }
 
-        public static List<long> GetOutput(long regA, long regB, long regC, int[] numbers)
+        public static List<int> GetOutput(long regA, long regB, long regC, int[] program)
         {
             var instructionPointer = 0;
-            var outputValues = new List<long>();
+            var outputValues = new List<int>();
 
             while (true)
             {
-                if (instructionPointer >= numbers.Length)
-                {
-                    // We're outside
+                if (instructionPointer >= program.Length)
                     break;
-                }
 
-                var opcode = numbers[instructionPointer];
-                var operand = numbers[instructionPointer + 1];
+                var opcode = program[instructionPointer];
+                var operand = program[instructionPointer + 1];
                 var shouldUpInsPointer = true;
 
                 switch (opcode)
                 {
-                    case 0: // Division
-                        regA /= (int)Math.Pow(2, GetComboOperand(operand, regA, regB, regC)); break;
+                    case 0:
+                        regA >>= (int)GetComboOperand(operand, regA, regB, regC); break;
                     case 1:
                         regB ^= operand; break;
                     case 2:
@@ -76,55 +68,16 @@ namespace AdventOfCode2024.Days
                         regB ^= regC; break;
                     case 5:
                         var output = GetComboOperand(operand, regA, regB, regC) % 8;
-                        outputValues.Add(output);
-                        break;
-                    case 6:
-                        regB = regA / (int)Math.Pow(2, GetComboOperand(operand, regA, regB, regC)); break;
-                    case 7:
-                        regC = regA / (int)Math.Pow(2, GetComboOperand(operand, regA, regB, regC)); break;
-                }
-
-                if (shouldUpInsPointer)
-                    instructionPointer += 2;
-            }
-
-            return outputValues;
-        }
-
-        private static List<int> GetOutputPart2(long regA, long regB, long regC, int[] numbers)
-        {
-            var outputValues = new List<int>();
-
-            for (int instructionPointer = 0; instructionPointer < numbers.Length; instructionPointer += 2)
-            {
-                var opcode = numbers[instructionPointer];
-                var operand = numbers[instructionPointer + 1];
-
-                switch (opcode)
-                {
-                    case 0:
-                        regA >>= (int)GetComboOperand(operand, regA, regB, regC); break;
-                    case 1:
-                        regB ^= operand; break;
-                    case 2:
-                        regB = GetComboOperand(operand, regA, regB, regC) % 8; break;
-                    case 3:
-                        if (regA == 0) break;
-                        instructionPointer = operand - 2;
-                        break;
-                    case 4:
-                        regB ^= regC; break;
-                    case 5:
-                        var output = (int)(GetComboOperand(operand, regA, regB, regC) % 8);
-                        outputValues.Add(output);
+                        outputValues.Add((int)output);
                         break;
                     case 6:
                         regB = regA >> (int)GetComboOperand(operand, regA, regB, regC); break;
                     case 7:
                         regC = regA >> (int)GetComboOperand(operand, regA, regB, regC); break;
-                    default:
-                        throw new Exception("Invalid opcode");
                 }
+
+                if (shouldUpInsPointer)
+                    instructionPointer += 2;
             }
 
             return outputValues;
@@ -145,9 +98,32 @@ namespace AdventOfCode2024.Days
             };
         }
 
-        private string[] GetInput()
+        private class InputData
         {
-            return FileInputAssistant.GetStringArrayFromFile(TextInputFilePath);
+            public long RegA { get; set; }
+            public long RegB { get; set; }
+            public long RegC { get; set; }
+            public int[] Program { get; set; } = [];
         }
+
+        private static InputData ParseInput(string[] input)
+        {
+            var programNumbers = input[4]
+                .Split("Program: ")[1]
+                .Split(",")
+                .Select(int.Parse)
+                .ToArray();
+
+            return new InputData
+            {
+                RegA = long.Parse(input[0].Split("Register A: ")[1]),
+                RegB = long.Parse(input[1].Split("Register B: ")[1]),
+                RegC = long.Parse(input[2].Split("Register C: ")[1]),
+                Program = programNumbers
+            };
+        }
+
+        private string[] GetInputFromFile()
+            => FileInputAssistant.GetStringArrayFromFile(TextInputFilePath);
     }
 }
