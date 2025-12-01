cfcd6f081003f7ea4f1e9c0eb7ea1adaf760fb90
Perry Jones <perry.jones@softwire.com>
2025-12-01 21:38:53 +0100

    2025: Add setup

---
+++ AdventOfCode2025/Days/Day.cs
@@ -0,0 +1,26 @@
+﻿using AdventOfCodeCommon;
+
+namespace AdventOfCode2025.Days
+{
+    internal class Day(bool isTest) : DayBase(0, isTest)
+    {
+        public override string Part1()
+        {
+            var input = GetInput();
+
+            return "Part 1";
+        }
+
+        public override string Part2()
+        {
+            var input = GetInput();
+
+            return "Part 2";
+        }
+
+        private string[] GetInput()
+        {
+            return FileInputAssistant.GetStringArrayFromFile(TextInputFilePath);
+        }
+    }
+}
