2ba729ae76d229109073e825c24498b53ad9adcd
Perry Jones <perry.jones@softwire.com>
2025-12-04 22:56:06 +0100

    Day03: Refactor

--- AdventOfCode2025/Days/Day03.cs
+++ AdventOfCode2025/Days/Day03.cs
@@ -4,78 +4,41 @@ namespace AdventOfCode2025.Days
 {
     internal class Day03(bool isTest) : DayBase(3, isTest)
     {
-        public override string Part1()
-        {
-            var input = GetInput();
+        public override string Part1() => GetJoltage(batteryCount: 2);
+        public override string Part2() => GetJoltage(batteryCount: 12);
 
-            var sum = 0;
-
-            foreach (var line in input)
-            {
-                // First, find the largest number, and its earliest index
-                var max = line[0];
-                var maxIndex = 0;
-                for (int i = 1; i < line.Length - 1; i++)
-                {
-                    if (line[i] > max)
-                    {
-                        max = line[i];
-                        maxIndex = i;
-                    }
-                }
-
-                // Then, find the largest number after that index
-                var secondMax = line[maxIndex + 1];
-                for (int i = maxIndex + 2; i < line.Length; i++)
-                {
-                    if (line[i] > secondMax)
-                    {
-                        secondMax = line[i];
-                    }
-                }
-
-                sum += 10 * max + secondMax;
-            }
-
-            return sum.ToString();
-        }
-
-        public override string Part2()
+        private string GetJoltage(int batteryCount)
         {
             var input = GetInput();
 
             var sum = 0L;
 
-            var NUM_DIGITS_REQUIRED = 12;
-
             foreach (var line in input)
             {
                 var currentDigit = 1;
                 var currentFromIndex = 0;
-                var digits = new List<int>();
+                var joltString = string.Empty;
 
-                while (currentDigit <= NUM_DIGITS_REQUIRED)
+                while (currentDigit <= batteryCount)
                 {
-                    // First, find the largest number, and its earliest index
                     var maxDigit = line[currentFromIndex];
                     var maxIndex = currentFromIndex;
 
-                    for (int i = currentFromIndex + 1; i < line.Length - NUM_DIGITS_REQUIRED + currentDigit; i++)
+                    for (int i = currentFromIndex + 1; i < line.Length - batteryCount + currentDigit; i++)
                     {
-                        if (line[i] > maxDigit)
-                        {
-                            maxDigit = line[i];
-                            maxIndex = i;
-                        }
+                        if (line[i] <= maxDigit)
+                            continue;
+
+                        maxDigit = line[i];
+                        maxIndex = i;
                     }
 
-                    digits.Add(maxDigit);
+                    joltString += maxDigit.ToString();
                     currentFromIndex = maxIndex + 1;
 
                     currentDigit++;
                 }
 
-                var joltString = string.Join("", digits);
                 var joltValue = long.Parse(joltString);
 
                 sum += joltValue;
@@ -85,8 +48,6 @@ namespace AdventOfCode2025.Days
         }
 
         private int[][] GetInput()
-        {
-            return FileInputAssistant.GetIntArrayRowsFromFile(TextInputFilePath);
-        }
+            => FileInputAssistant.GetIntArrayRowsFromFile(TextInputFilePath);
     }
 }
