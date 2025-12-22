b5a153d5eeb525bb56fca11f2a2d9e2dff8655a4
Perry Jones <perry.jones@yuki.nl>
2025-12-22 23:40:00 +0100

    Day01: Both parts done

---
+++ AdventOfCode2015/Days/Day01.cs
@@ -0,0 +1,37 @@
+﻿using AdventOfCodeCommon;
+
+namespace AdventOfCode2015.Days
+{
+    internal class Day01(bool isTest) : DayBase(1, isTest)
+    {
+        public override string Part1()
+        {
+            var input = GetInput();
+            var openBracketCount = input.Count(c => c == '(');
+            var closeBracketCount = input.Count(c => c == ')');
+
+            var difference = openBracketCount - closeBracketCount;
+
+            return difference.ToString();
+        }
+
+        public override string Part2()
+        {
+            var input = GetInput();
+            var difference = 0;
+
+            foreach (var (c, index) in input.Select((c, i) => (c, i)))
+            {
+                difference += c == '(' ? 1 : -1;
+
+                if (difference == -1)
+                    return (index + 1).ToString();
+            }
+
+            throw new InvalidOperationException("Basement not reached");
+        }
+
+        private string GetInput()
+            => FileInputAssistant.GetStringFromFile(TextInputFilePath);
+    }
+}
